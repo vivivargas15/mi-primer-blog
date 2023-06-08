47e9905c707413bbec19859d679e478d487b048a
vivivargas15 <viviana_vargas15@hotmail.com>
2023-06-08 15:53:02 -0500

    add image in models

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image=models.ImageField(upload_to='blog', null=True, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
